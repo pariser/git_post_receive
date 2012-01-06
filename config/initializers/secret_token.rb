486687181f3ab9c41281652d08cab70eca188157
Andrew Pariser <pariser@gmail.com>
2012-01-06 09:41:19 -0800

    Starting this project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitPostCommit::Application.config.secret_token = 'b351af00eee1b8f8d882bac8cdbcf2d12fc0adcfacb65512d4a73a5d50dfdf2d42ffb56ad62baaa4e72f8645496a4fafb86f5d0b1267560190f712821fdfa9c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GitPostCommit::Application.config.session_store :cookie_store, :key => '_git_post_commit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GitPostCommit::Application.config.session_store :active_record_store
